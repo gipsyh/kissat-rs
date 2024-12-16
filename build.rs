b728840a12e2a4b150186b2b586c01206d52ea8f
Yuheng Su <gipsyh.icu@gmail.com>
2024-12-16 10:39:44 +0000

    v0.4.3

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 #![feature(exit_status_error)]
 
-use std::path::PathBuf;
+use giputils::build::copy_build;
+use std::env;
 use std::process::Command;
-use std::{env, fs};
 extern crate giputils;
 
 fn main() -> Result<(), String> {
@@ -11,33 +11,26 @@ fn main() -> Result<(), String> {
         .status()
         .unwrap();
     println!("cargo:rerun-if-changed=./kissat");
-    let src_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let kissat_src = PathBuf::from(src_dir).join("kissat");
-    let kissat_out = PathBuf::from(out_dir).join("kissat");
-    let num_jobs = env::var("NUM_JOBS").unwrap();
-    if kissat_out.exists() {
-        fs::remove_dir_all(&kissat_out).unwrap();
-    }
-    fs::create_dir(&kissat_out).unwrap();
-    let overlay = giputils::mount::MountOverlay::new(&kissat_src, &kissat_out);
-    Command::new("sh")
-        .arg("configure")
-        .current_dir(overlay.path())
-        .status()
-        .map_err(|e| e.to_string())?
-        .exit_ok()
-        .map_err(|e| e.to_string())?;
-    Command::new("make")
-        .arg(format!("-j{}", num_jobs))
-        .current_dir(overlay.path())
-        .status()
-        .map_err(|e| e.to_string())?
-        .exit_ok()
-        .map_err(|e| e.to_string())?;
+    let cb_path = copy_build("kissat", |src| {
+        Command::new("sh")
+            .arg("configure")
+            .current_dir(src)
+            .status()
+            .map_err(|e| e.to_string())?
+            .exit_ok()
+            .map_err(|e| e.to_string())?;
+        let num_jobs = env::var("NUM_JOBS").unwrap();
+        Command::new("make")
+            .arg(format!("-j{}", num_jobs))
+            .current_dir(src)
+            .status()
+            .map_err(|e| e.to_string())?
+            .exit_ok()
+            .map_err(|e| e.to_string())
+    })?;
     println!(
         "cargo:rustc-link-search=native={}",
-        kissat_out.join("build").display()
+        cb_path.join("build").display()
     );
     println!("cargo:rustc-link-lib=static=kissat");
     println!("cargo:rustc-link-lib=dylib=stdc++");
